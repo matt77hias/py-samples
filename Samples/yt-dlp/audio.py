ada322037bbef86d4043f5d8636c60984cdd353c
Matthias Moulin <matthias.moulin@gmail.com>
2023-08-09 21:49:05 +0200

    Update audio.py

--- Samples/yt-dlp/audio.py
+++ Samples/yt-dlp/audio.py
@@ -2,18 +2,27 @@ import yt_dlp
 
 urls = ['']
 
+hidden_files = False
+
 ydl_opts = {
     'format': 'm4a/bestaudio/best',
+    'ignoreerrors': hidden_files,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'm4a',
     }]
 }
 
-while(True):
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            error_code = ydl.download(urls)
-        break
-    except:
-        pass
+def download():
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(urls)
+
+if hidden_files:
+    download()
+else:
+  while(True):
+      try:
+          download()
+          break
+      except:
+          pass
