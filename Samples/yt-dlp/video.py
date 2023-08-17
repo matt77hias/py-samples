e5fa2080530e5005283bb75877fc3cc4b7fa1998
Matthias Moulin <matthias.moulin@gmail.com>
2023-08-17 20:51:19 +0200

    Update video.py

--- Samples/yt-dlp/video.py
+++ Samples/yt-dlp/video.py
@@ -6,5 +6,8 @@ ydl_opts = {
     'format_sort': ['ext:mp4:m4a']
 }
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    error_code = ydl.download(urls)
+def download():
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(urls)
+
+download()
