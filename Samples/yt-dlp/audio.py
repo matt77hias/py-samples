7d6d5d18ba97b017fd560ce004e1db57d39a4ad6
Matthias Moulin <matthias.moulin@gmail.com>
2023-08-23 20:59:57 +0200

    Update audio.py

--- Samples/yt-dlp/audio.py
+++ Samples/yt-dlp/audio.py
@@ -10,8 +10,10 @@ ydl_opts = {
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'm4a',
-    }]
+    }] #{ 'key': 'FFmpegSplitChapters' }
 }
+
+#print(yt_dlp.YoutubeDL.__doc__)
 
 def download():
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
